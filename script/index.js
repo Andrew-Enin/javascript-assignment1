aea8779932948b9f8b11a9fb85f15470da69f73d
Andrew Enin <eninandy@gmail.com>
2024-01-20 00:15:47 +0100

    First commit

---
+++ script/index.js
@@ -0,0 +1,77 @@
+//IF ELSE
+// (1) Age EligibilityC hecker:Determin if abperson is eligible to vote based on their age.
+let recommendedAge = 18;
+const userAge = Number(prompt("Enter Your Age"));
+
+if (userAge >= recommendedAge) {
+    alert(`You are ${userAge} years old and eligible to vote`);
+} else {
+    alert(`You are ${userAge} years old and not eligible to vote, kindly wait until you are ${recommendedAge} or greater`);
+}
+
+
+
+// (2) Even or Odd Number Identifier: Check if a given number is even oroddand display a message.
+const inputNumber = Number(prompt("Enter Number"));
+
+if (enterNumber % 2 === 0) {
+    alert(`You just entered ${inputNumber} which is an even number`);
+} else {
+    alert(`You entered ${enterNumber} which is an odd number`);
+}
+
+
+
+// (3) Vowel or Consonant Classifier: Determine if a given letter is a vowel or a consonant.
+let letterOne = "a";
+let letterTwo = "e";
+let letterThree = "i";
+let letterFour = "o";
+let letterFive = "u";
+
+const enterNumber = prompt("Enter Number");
+
+if (enterNumber === letterOne) {
+    alert(`You just entered letter '${enterNumber}' which is a vowel`);
+}else if (enterNumber === letterTwo) {
+    alert(`You just entered letter '${enterNumber}' which is a vowel`);
+}else if (enterNumber === letterThree) {
+    alert(`You just entered letter '${enterNumber}' which is a vowel`);
+}else if (enterNumber === letterFour) {
+    alert(`You just entered letter '${enterNumber}' which is a vowel`);
+}else if (enterNumber === letterFive) {
+    alert(`You just entered letter '${enterNumber}' which is a vowel`);
+}else {
+    alert(`The text you entered is letter '${enterNumber}' and it is a consonant`);
+}
+
+
+
+// (4) Largest Number Finder:Compare two numbers and return the larger one.
+
+
+
+
+
+
+
+// (5) Simple Calculator: Create a calculator that can perform basic arithmetic
+// let addnumbers = Number(prompt("Add Numbers"))
+// let addnumbersTwo = Number(prompt("Add Numbers"))
+// function sum (num1, num2) {
+//     let add = num1 + num2
+//     console.log(add)
+//     return add
+// }
+
+// let total = sum(addnumbers, addnumbersTwo)
+// alert(`You entered: ${total}`)
+
+// let name1 = "Andrew";
+
+// for(let names of name1) {
+//     console.log(`This is my ${names}`)
+// }
+
+
+
